0be8543d92685ef5c9c8a630b57477721ab2494d
Kamajor <kamajorqa@gmail.com>
2023-09-25 15:18:04 +0300

    Реализована функция удаления поста. Использована кнопка ScrollbackToTop. В карточку добавлена кнопка удаления конкретного поста. Исправлен шаблон главной страницы. Добавлен массив зависимостей в слушатель получения карточек с сервера для исключения бесконечного цикла загрузки постов.

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,44 @@
-import { Card } from 'antd';
-import Meta from 'antd/es/card/Meta';
+import { Button, Card, Tooltip } from 'antd';
+import { FiDelete } from 'react-icons/fi';
 
-// const { Meta } = Card;
+const { Meta } = Card;
 
-function PostCard({ postTitle, imgUrl, postContent }) {
+function PostCard({ postTitle, imgUrl, postContent, id, handleDeletePost }) {
   return (
     <Card
-      hoverable
-      cover={
-        <img
-          alt={postTitle}
-          src={imgUrl}
-          style={{
-            marginBottom: '1rem',
-          }}
-        />
-      }
+      style={{
+        maxWidth: '50%',
+        textAlign: 'center',
+        position: 'relative',
+      }}
+      cover={<img alt={postTitle} src={imgUrl} />}
     >
       <Meta title={postTitle} />
       <p>{postContent}</p>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          right: -40,
+        }}
+      >
+        <Tooltip title="Delete this blog post">
+          <Button
+            shape="circle"
+            type="dashed"
+            danger
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: ' center',
+              justifySelf: 'flex-end',
+            }}
+            ghost
+            icon={<FiDelete />}
+            onClick={handleDeletePost(id)}
+          />
+        </Tooltip>
+      </div>
     </Card>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Button, Col, Layout, Row } from 'antd';
-import { getDocs, collection } from 'firebase/firestore';
+import { Button, FloatButton, Layout } from 'antd';
+import {
+  getDocs,
+  collection,
+  onSnapshot,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
+import { AiOutlineVerticalAlignTop } from 'react-icons/ai';
 
 import { db } from '../firebase';
 import { TitleComponent } from '../components/TitleComponent';
@@ -11,16 +18,37 @@ function HomePage() {
   const [postsList, setPostsList] = useState([]);
   const postsCollectionRef = collection(db, 'posts');
 
-  useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      // console.log(data);
-      setPostsList(
-        data.docs.map((document) => ({ ...document.data(), id: document.id }))
-      );
-    };
+  // useEffect(() => {
+  //   onSnapshot(postsCollectionRef, (snapshot) => {
+  //     console.log(snapshot.docs);
+  //     const data = snapshot.docs.map((doc) => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data(),
+  //       };
+  //     });
+  //     console.log(data);
+  //     setPostsList(data);
+  //   });
+  // }, []);
+
+  const getPosts = async () => {
+    const data = await getDocs(postsCollectionRef);
+    console.log(data);
+    setPostsList(
+      data.docs.map((document) => ({ ...document.data(), id: document.id }))
+    );
+  };
+
+  const handleDeletePost = async (id) => {
+    const postDoc = doc(db, 'posts', id);
+    await deleteDoc(postDoc);
     getPosts();
-  });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <Content
@@ -30,16 +58,28 @@ function HomePage() {
     >
       <TitleComponent level={2}>Blog Posts</TitleComponent>
 
-      <main className="contentWrapper">
-        <h1>Съешь еще этих мягких французских булок</h1>
-        <Row justify="center" gutter={{ xs: 4, sm: 8, md: 12, lg: 16 }}>
-          <Col span={12}>
-            {postsList.map((post) => {
-              return <PostCard {...post} key={post.id} />;
-            })}
-          </Col>
-        </Row>
+      <main
+        className="contentWrapper"
+        style={{
+          display: 'flex',
+          gap: '1rem',
+          alignItems: 'center',
+        }}
+      >
+        {postsList.map((post) => {
+          return (
+            <PostCard
+              {...post}
+              key={post.id}
+              handleDeletePost={handleDeletePost}
+            />
+          );
+        })}
       </main>
+      <FloatButton.BackTop
+        icon={<AiOutlineVerticalAlignTop />}
+        tooltip={<div>Scroll back to top</div>}
+      />
       <Button
         type="primary"
         style={{
