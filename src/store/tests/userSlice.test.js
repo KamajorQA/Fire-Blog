526a48247f54b03309f05abbe6915f9007ec90c2
Kamajor <kamajorqa@gmail.com>
2023-10-06 20:36:56 +0300

    Реализованы тесты синхронных Redux экшнов.

--- src/store/tests/userSlice.test.js
+++ src/store/tests/userSlice.test.js
@@ -6,9 +6,34 @@ const matchingState = {
   uid: null,
 };
 
+const mockUser = {
+  email: 'kj@gmail.com',
+  displayName: 'jakar',
+  uid: 'u007',
+};
+
 describe('userSlice', () => {
   it('should return default state when passed an empty action', () => {
     const result = userReducer(undefined, { type: '' });
+
+    expect(result).toEqual(matchingState);
+  });
+
+  it('should set current user to the store with "setUser" action', () => {
+    const action = { type: setUser.type, payload: mockUser };
+
+    const result = userReducer(matchingState, action);
+
+    expect(result.displayName).toBe('jakar');
+    expect(result.email).toBe('kj@gmail.com');
+    expect(result.uid).toBe('u007');
+  });
+
+  it('should set user state to it\'s initial value with "removeUser" action', () => {
+    const user = mockUser;
+    const action = { type: removeUser.type };
+
+    const result = userReducer(user, action);
 
     expect(result).toEqual(matchingState);
   });
